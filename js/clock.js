2909ba5677ca44b5095ce214920fba844bbefedf
ri-ca4 <riley.de12@gmail.com>
2022-04-26 16:44:44 -0400

    fixed display location of hrs and mins

--- js/clock.js
+++ js/clock.js
@@ -10,17 +10,16 @@
 FUNCTIONS TO DISPLAY CURRENT TIME ON CLOCK FACE
 */
 
-var dispMin = document.getElementById('hrs');
-var dispHrs = document.getElementById('mins');
+var dispMin  = document.getElementById('mins');
+var dispHrs  = document.getElementById('hrs');
 var dispAmPm = document.getElementById('amPM');
 
 window.onload = dispClock;
 
 //sync interval with current time by starting interval at the start of the next minute
-var now = new Date();
-console.log(now);
-var sec = now.getSeconds();
-var offSet = ((60 - sec) * 1000);
+var now    = new Date();
+var sec    = now.getSeconds();
+var offSet = ((60 - sec) * 1000); //the number of milliseconds before the new minute
 
 var startTime = setTimeout(refresh, offSet);
 
@@ -31,7 +30,7 @@ function refresh() {
 
 function dispClock(){
     var now = new Date();
-    //console.log(now);//console.log confirmed interval starts on the new minute
+        //console.log(now);//console.log confirmed interval starts on the new minute
     var hour = now.getHours();
     var pm;
     //set to AM/PM format and determine if PM is true
